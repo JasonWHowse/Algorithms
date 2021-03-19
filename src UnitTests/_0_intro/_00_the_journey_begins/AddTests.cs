1feb0fba51e723aba184a8c59287a644a15c07ef
Jason <JasonWHowse@gmail.com>
2021-03-18 21:34:48 -0500

    Adding C# solutions

---
+++ src UnitTests/_0_intro/_00_the_journey_begins/AddTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.src._0_intro._00_the_journey_begins;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.src._0_intro._00_the_journey_begins.Tests {
+    [TestClass()]
+    public class AddTests {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test1() {
+            int testCase1 = 1;
+            int testCase2 = 2;
+            int solution = 3;
+            test(testCase1, testCase2, solution);
+        }//public void test1() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test2() {
+            int testCase1 = 0;
+            int testCase2 = 1000;
+            int solution = 1000;
+            test(testCase1, testCase2, solution);
+        }//public void test2() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test3() {
+            int testCase1 = 2;
+            int testCase2 = -39;
+            int solution = -37;
+            test(testCase1, testCase2, solution);
+        }//public void test3() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test4() {
+            int testCase1 = 99;
+            int testCase2 = 100;
+            int solution = 199;
+            test(testCase1, testCase2, solution);
+        }//public void test4() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test5() {
+            int testCase1 = -100;
+            int testCase2 = 100;
+            int solution = 0;
+            test(testCase1, testCase2, solution);
+        }//public void test5() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test6() {
+            int testCase1 = -1000;
+            int testCase2 = -1000;
+            int solution = -2000;
+            test(testCase1, testCase2, solution);
+        }//public void test6() {
+
+        private void test(int testCase1,int testCase2,int solution) {
+            Assert.AreEqual(solution,new Add().add(testCase1,testCase2));
+        }//private void test(int testCase1,int testCase2,int solution) {
+    }//public class AddTests {
+}//namespace Algorithms.src._0_intro._00_the_journey_begins.Tests {
---
+++ src/_0_intro/_00_the_journey_begins/Add.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms._0_intro._00_the_journey_begins {
+    public class Add {
+
+        /*
+         Write a function that returns the sum of two numbers.
+
+    Example
+
+    For param1 = 1 and param2 = 2, the output should be
+    add(param1, param2) = 3.
+
+        Input/Output
+
+    [execution time limit] 3 seconds (cs)
+
+    [input] integer param1
+
+    Guaranteed constraints:
+    -1000 ≤ param1 ≤ 1000.
+
+    [input] integer param2
+
+    Guaranteed constraints:
+    -1000 ≤ param2 ≤ 1000.
+
+    [output] integer
+
+    The sum of the two inputs.
+         */
+
+        public int add(int param1, int param2) {
+            return param1 + param2;
+        }//public int add(int param1, int param2) {
+    }//public class Add {
+}//namespace Algorithms.src._0_intro._00_the_journey_begins {
