e7de4fb6737cf4f80af2aba7b0d251fb82831f09
Jason <JasonWHowse@gmail.com>
2021-03-19 10:36:00 -0500

    update

---
+++ src UnitTests/_0_intro/_00_the_journey_begins/CenturyFromYearTests.cs
@@ -0,0 +1,84 @@
+﻿using Algorithms.src._0_intro._00_the_journey_begins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms._0_intro._00_the_journey_begins {
+    [TestClass()]
+    public class CenturyFromYearTests {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test1() {
+            int testCase = 1905;
+            int solution = 20;
+            test(testCase, solution);
+        }//public void test1() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test2() {
+            int testCase = 1700;
+            int solution = 17;
+            test(testCase, solution);
+        }//public void test2() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test3() {
+            int testCase = 1988;
+            int solution = 20;
+            test(testCase, solution);
+        }//public void test3() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test4() {
+            int testCase = 2000;
+            int solution = 20;
+            test(testCase, solution);
+        }//public void test4() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test5() {
+            int testCase = 2001;
+            int solution = 21;
+            test(testCase, solution);
+        }//public void test5() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test6() {
+            int testCase = 200;
+            int solution = 2;
+            test(testCase, solution);
+        }//public void test6() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test7() {
+            int testCase = 374;
+            int solution = 4;
+            test(testCase, solution);
+        }//public void test7() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test8() {
+            int testCase = 45;
+            int solution = 1;
+            test(testCase, solution);
+        }//public void test8() {
+
+
+        [TestMethod(), Timeout(3000)]
+        public void test9() {
+            int testCase = 8;
+            int solution = 1;
+            test(testCase, solution);
+        }//public void test9() {
+
+        private void test(int testCase, int solution) {
+            Assert.AreEqual(solution, new CenturyFromYear().centuryFromYear(testCase));
+        }//private void test(int testCase, int solution) {
+    }//public class CenturyFromYearTests {
+}//namespace Algorithms._0_intro._00_the_journey_begins {
---
+++ src/_0_intro/_00_the_journey_begins/CenturyFromYear.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.src._0_intro._00_the_journey_begins {
+    public class CenturyFromYear {
+        /*
+
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+Example
+
+For year = 1905, the output should be
+centuryFromYear(year) = 20;
+For year = 1700, the output should be
+centuryFromYear(year) = 17.
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] integer year
+
+A positive integer, designating the year.
+
+Guaranteed constraints:
+1 ≤ year ≤ 2005.
+
+[output] integer
+
+The number of the century the year is in.
+
+         */
+        public int centuryFromYear(int year) {
+            return (year - 1) / 100 + 1;
+        }//public int centuryFromYear(int year) {
+    }//public class CenturyFromYear {
+}//namespace Algorithms.src._0_intro._00_the_journey_begins {
