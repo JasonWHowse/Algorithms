ef0bb02215fad389132c08d6f036d755a70961a6
Jason <JasonWHowse@gmail.com>
2021-03-21 18:09:34 -0500

    Diving Deeper

---
+++ src UnitTests/_0_intro/_07_diving_deeper/DifferentSymbolsNaiveTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms.src._0_intro._07_diving_deeper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._0_intro._07_diving_deeper {
+    [TestClass()]
+    public class DifferentSymbolsNaiveTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = "cabca";
+            var solution = 3;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = "aba";
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = "ccccccccccc";
+            var solution = 1;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = "bcaba";
+            var solution = 3;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = "codesignal";
+            var solution = 10;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        private void Test(string testCase, int solution) {
+            Assert.AreEqual(solution, new DifferentSymbolsNaive().differentSymbolsNaive(testCase));
+        }//TODO:fix
+    }//public class DifferentSymbolsNaiveTests {
+}//namespace Algorithms.src_UnitTests._0_intro._07_diving_deeper {
---
+++ src/_0_intro/_07_diving_deeper/ArrayMaxConsecutiveSum.cs
@@ -0,0 +1,57 @@
+﻿namespace Algorithms.src._0_intro._07_diving_deeper {
+    public class ArrayMaxConsecutiveSum {
+
+        /*
+
+Given array of integers, find the maximal possible sum of some of its k consecutive elements.
+
+Example
+
+For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
+arrayMaxConsecutiveSum(inputArray, k) = 8.
+All possible sums of 2 consecutive elements are:
+
+2 + 3 = 5;
+3 + 5 = 8;
+5 + 1 = 6;
+1 + 6 = 7.
+Thus, the answer is 8.
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] array.integer inputArray
+
+Array of positive integers.
+
+Guaranteed constraints:
+3 ≤ inputArray.length ≤ 105,
+1 ≤ inputArray[i] ≤ 1000.
+
+[input] integer k
+
+An integer (not greater than the length of inputArray).
+
+Guaranteed constraints:
+1 ≤ k ≤ inputArray.length.
+
+[output] integer
+
+The maximal possible sum.
+
+        */
+
+        public
+        int arrayMaxConsecutiveSum(int[] inputArray, int k) {
+            int maxVal = 0;
+            for (int i = --k; i < inputArray.Length; i++) {
+                int currentVal = 0;
+                for (int j = i - k; j <= i; j++) {
+                    currentVal += inputArray[j];
+                }//for (int j = i - k; j <= i; j++) {
+                maxVal = System.Math.Max(maxVal, currentVal);
+            }//for (int i = --k; i < inputArray.Length; i++) {
+            return maxVal;
+        }//int arrayMaxConsecutiveSum(int[] inputArray, int k) {
+    }//public class ArrayMaxConsecutiveSum {
+}//namespace Algorithms.src._0_intro._07_diving_deeper {
