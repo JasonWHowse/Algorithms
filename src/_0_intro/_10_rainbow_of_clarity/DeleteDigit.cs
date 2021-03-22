18e798a8eef33529c24287f9fde34a6dfc06fed0
Jason <JasonWHowse@gmail.com>
2021-03-22 10:26:27 -0500

    Rainbow of Clarity

---
+++ src/_0_intro/_10_rainbow_of_clarity/DeleteDigit.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.src._0_intro._10_rainbow_of_clarity {
+    public class DeleteDigit {
+
+        /*
+
+Given some integer, find the maximal number you can obtain by deleting exactly one digit of the given number.
+
+Example
+
+For n = 152, the output should be
+deleteDigit(n) = 52;
+For n = 1001, the output should be
+deleteDigit(n) = 101.
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] integer n
+
+Guaranteed constraints:
+10 ≤ n ≤ 106.
+
+[output] integer
+
+        */
+
+        public
+        int deleteDigit(int n) {
+            int max = 0;
+            for (int i = 1; i < n; i *= 10) {
+                max = System.Math.Max(max, (n % (i)) + ((n / (i * 10)) * i));
+            }//for(int i=1;i<n;i*=10){
+            return max;
+        }//int deleteDigit(int n) {
+    }//public class DeleteDigit {
+}//namespace Algorithms.src._0_intro._10_rainbow_of_clarity {
