db0796a00f2a95cbe036003db44a3ec88f3a0189
Jason <JasonWHowse@gmail.com>
2021-03-23 13:34:06 -0500

    Corner of 0s and 1s

---
+++ src UnitTests/_1_the_core/_02_corner_of_0s_and_1s/ArrayPackingTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms.src._1_the_core._02_corner_of_0s_and_1s;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._02_corner_of_0s_and_1s {
+    [TestClass()]
+    public class ArrayPackingTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            int[] testCase = { 24, 85, 0 };
+            var solution = 21784;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            int[] testCase = { 23, 45, 39 };
+            var solution = 2567447;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            int[] testCase = { 1, 2, 4, 8 };
+            var solution = 134480385;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            int[] testCase = { 5 };
+            var solution = 5;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            int[] testCase = { 187, 99, 42, 43 };
+            var solution = 724198331;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        private void Test(int[] testCase, int solution) {
+            Assert.AreEqual(solution, new ArrayPacking().arrayPacking(testCase));
+        }//private void Test(int[] testCase,int solution) {
+    }//public class ArrayPackingTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._02_corner_of_0s_and_1s {
---
+++ src UnitTests/_1_the_core/_02_corner_of_0s_and_1s/SecondRightmostZeroBitTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms.src._1_the_core._02_corner_of_0s_and_1s;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._02_corner_of_0s_and_1s {
+    [TestClass()]
+    public class SecondRightmostZeroBitTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = 37;
+            var solution = 8;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = 1073741824;
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = 83748;
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = 4;
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = 728782938;
+            var solution = 4;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        private void Test(int testCase, int solution) {
+            Assert.AreEqual(solution, new SecondRightmostZeroBit().secondRightmostZeroBit(testCase));
+        }//private void Test(int testCase,int solution) {
+    }//public class SecondRightmostZeroBitTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._02_corner_of_0s_and_1s {
