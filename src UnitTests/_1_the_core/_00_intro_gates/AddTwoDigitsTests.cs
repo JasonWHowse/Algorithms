812cf3e689d8556d8bd3fcf1a0f93d09868d338c
Jason <JasonWHowse@gmail.com>
2021-03-22 21:24:20 -0500

    Intro Gates

---
+++ src UnitTests/_1_the_core/_00_intro_gates/AddTwoDigitsTests.cs
@@ -0,0 +1,84 @@
+﻿using Algorithms.src._1_the_core._00_intro_gates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._00_intro_gates {
+    [TestClass()]
+    public class AddTwoDigitsTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = 29;
+            var solution = 11;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = 48;
+            var solution = 12;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = 10;
+            var solution = 1;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = 25;
+            var solution = 7;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = 52;
+            var solution = 7;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = 99;
+            var solution = 18;
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase = 44;
+            var solution = 8;
+            Test(testCase, solution);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase = 50;
+            var solution = 5;
+            Test(testCase, solution);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase = 39;
+            var solution = 12;
+            Test(testCase, solution);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase = 26;
+            var solution = 8;
+            Test(testCase, solution);
+        }//public void Test10() {
+
+        private void Test(int testCase, int solution) {
+            Assert.AreEqual(solution, new AddTwoDigits().addTwoDigits(testCase));
+        }//private void Test(int testCase,int solution) {
+    }//public class AddTwoDigitsTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._00_intro_gates {
---
+++ src/_1_the_core/_00_intro_gates/AddTwoDigits.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms.src._1_the_core._00_intro_gates {
+    public class AddTwoDigits {
+
+        /*
+
+You are given a two-digit integer n. Return the sum of its digits.
+
+Example
+
+For n = 29, the output should be
+addTwoDigits(n) = 11.
+
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] integer n
+
+A positive two-digit integer.
+
+Guaranteed constraints:
+10 ≤ n ≤ 99.
+
+[output] integer
+
+The sum of the first and second digits of the input number.
+
+        */
+
+        public
+        int addTwoDigits(int n) {
+            return (n / 10) + (n % 10);
+        }//int addTwoDigits(int n) {
+    }//public class AddTwoDigits {
+}//namespace Algorithms.src._1_the_core._00_intro_gates {
