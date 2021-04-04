dc151456ddf7714e117b8b5e61ff95991cc9ae8a
Jason <JasonWHowse@gmail.com>
2021-04-04 00:02:34 -0500

    Update for Helpers Algorithm

--- src UnitTests/helper_functions/HelpersTest.java
+++ src UnitTests/helper_functions/HelpersTest.java
@@ -6,8 +6,8 @@ package helper_functions;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HelpersTest {
 
@@ -50,8 +50,6 @@ class HelpersTest {
         MatcherAssert.assertThat(testCase,is(solution));
     }//void test3() {
 
-
-
     @Test
     void test4() {//tests 3d the generic as String
         String[][][] testCase=new String[][][]{{   {"asdf","sdf","sde","s"},
@@ -62,12 +60,97 @@ class HelpersTest {
         String[][][] solution=new String[][][]{{   {"asdf","sdf","sde","s"},
                 {"0","5","0","0"},
                 {"2","0","3","3"}},{   {"0","1","2","3"},
-            {"0","5","0","0"},
-            {"2","0","3","3"}}};
+                {"0","5","0","0"},
+                {"2","0","3","3"}}};
         mutator(Helpers.deepCopy(testCase));
 //        mutator((testCase[0]));//tests that array is mutated
         MatcherAssert.assertThat(testCase,is(solution));
-    }//void test3() {
+    }//void test4() {
+
+    @Test
+    void test5() {//tests the primitive as int
+        Integer[][] testCase=new Integer[][]{   {0,1,1,2},
+                {0,5,0,0},
+                {2,0,3,3}};
+        Integer[][] solution=new Integer[][]{   {0,1,1,2},
+                {0,5,0,0},
+                {2,0,3,3}};
+        mutator(Helpers.deepCopy(testCase));
+//        mutator((testCase));//tests that array is mutated
+        MatcherAssert.assertThat(testCase,is(solution));
+    }//void test5() {
+
+    @Test
+    void test6() {//tests the generic as Integer
+        Integer[][] testCase=new Integer[][]{   {0,1,1,2},
+                {0,5,0,0},
+                {2,0,3,3}};
+        Integer[][] solution=new Integer[][]{   {0,1,1,2},
+                {0,5,0,0},
+                {2,0,3,3}};
+        mutator(Helpers.deepCopy(testCase));
+//        mutator((testCase));//tests that array is mutated
+        MatcherAssert.assertThat(testCase,is(solution));
+    }//void test6() {
+
+    @Test
+    void test7() {//tests the generic as String
+        String[][] testCase=new String[][]{   {"asdf","sdf","sde","s"},
+                {"0","5","0","0"},
+                {"2","0","3","3"}};
+        String[][] solution=new String[][]{   {"asdf","sdf","sde","s"},
+                {"0","5","0","0"},
+                {"2","0","3","3"}};
+        mutator(Helpers.deepCopy(testCase));
+//        mutator((testCase));//tests that array is mutated
+        MatcherAssert.assertThat(testCase,is(solution));
+    }//void tes7() {
+
+    @Test
+    void test8() {//tests 3d the generic as String
+        String[][][] testCase=new String[][][]{{   {"asdf","sdf","sde","s"},
+                {"0","5","0","0"},
+                {"2","0","3","3"}},{   {"0","1","2","3"},
+                {"0","5","0","0"},
+                {"2","0","3","3"}}};
+        String[][][] solution=new String[][][]{{   {"asdf","sdf","sde","s"},
+                {"0","5","0","0"},
+                {"2","0","3","3"}},{   {"0","1","2","3"},
+                {"0","5","0","0"},
+                {"2","0","3","3"}}};
+        mutator(Helpers.deepCopy(testCase));
+//        mutator((testCase));//tests that array is mutated
+        MatcherAssert.assertThat(testCase,is(solution));
+    }//void test8() {
+
+    @Test
+    void test9() {//tests 4d the generic as Integer
+        Integer[][][][] testCase =new Integer[5][5][5][5];
+        for(int x=0;x<testCase.length;x++){
+            for(int y=0;y<testCase[x].length;y++){
+                for(int z=0;z<testCase[x][y].length;z++){
+                    for(int i=0;i<testCase[x][y][z].length;i++){
+                        testCase[x][y][z][i]=x*y+x*z+y*z+x*i+y*i+z*i;
+                    }
+                }
+            }
+        }
+        Integer[][][][] solution =new Integer[5][5][5][5];
+        for(int x=0;x<solution.length;x++){
+            for(int y=0;y<solution[x].length;y++){
+                for(int z=0;z<solution[x][y].length;z++){
+                    for(int i=0;i<solution[x][y][z].length;i++){
+                        solution[x][y][z][i]=x*y+x*z+y*z+x*i+y*i+z*i;
+                    }
+                }
+            }
+        }
+        mutator(Helpers.deepCopy(testCase));
+//        mutator((testCase));//tests that array is mutated
+        MatcherAssert.assertThat(testCase,is(solution));
+    }//void test9() {
+
+
 
     private void mutator(int[][] toMutate){
         for (int i=0;i<toMutate.length;i++){
@@ -98,6 +181,18 @@ class HelpersTest {
             for (int j=0;j<toMutate[i].length;j++){
                 for(int k=0;k<toMutate[i][j].length;k++) {
                     toMutate[i][j][k] = toMutate[i][j][k] + "1";
+                }
+            }//for (int i=0;i<toMutate.length;i++){
+        }//for (int i=0;i<toMutate.length;i++){
+    }//private void mutator(String[][] toMutate){
+
+    private void mutator(Integer[][][][] toMutate){
+        for (int i=0;i<toMutate.length;i++){
+            for (int j=0;j<toMutate[i].length;j++){
+                for(int k=0;k<toMutate[i][j].length;k++) {
+                    for(int l=0;l<toMutate[i][j][k].length;l++) {
+                        toMutate[i][j][k][l] = toMutate[i][j][k][l] + (Integer) 1;
+                    }
                 }
             }//for (int i=0;i<toMutate.length;i++){
         }//for (int i=0;i<toMutate.length;i++){
--- src/helper_functions/Helpers.java
+++ src/helper_functions/Helpers.java
@@ -4,57 +4,35 @@
 
 package helper_functions;
 
-import java.lang.reflect.Array;
-import java.util.Collection;
-
 public class Helpers{
-    public static <E> E[][] deepCopy (E[][] original){
-        E[][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=original[i].clone();
-        }//for(int i=0;i<original.length;i++){
-        return copy;
-    }//public static <E> E[][] deepCopy (E[][] original){
-
-    public static <E> E[][][] deepCopy (E[][][] original){
-        E[][][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=deepCopy(original[i]).clone();
-        }//public static <E> E[][][] deepCopy (E[][][] original){
-        return copy;
-    }//public static <E> E[][][] deepCopy (E[][][] original){
-
-    public static int[][] deepCopy (int[][] original){
-        int[][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=original[i].clone();
+    public static int[][] deepCopy(int[][] original) {
+        int[][] copy = original.clone();
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
         }//for(int i=0;i<original.length;i++){
         return copy;
     }//public static int[][] deepCopy (int[][] original){
 
-    public static boolean[][] deepCopy (boolean[][] original){
-        boolean[][] copy= original.clone();
-        for(int i=0;i<original.length;i++){
-            copy[i]=original[i].clone();
+    public static boolean[][] deepCopy(boolean[][] original) {
+        boolean[][] copy = original.clone();
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
         }//for(int i=0;i<original.length;i++){
         return copy;
     }//public static boolean[][] deepCopy (boolean[][] original){
 
-//potential for deepcopy recursive top layer array method explore later
-//    public static Object[] deepCopy1 (Object[] original){
-//        Object[] copy= original[0].clone();
-//        if(original[0] instanceof Collection<?>){
-//            for(int i=0;i< original[0].length;i++){
-//                copy[i]=deepCopy1(new Object[] {original[0][i]});
-//            }
-//        }
-//        for(int i=0;i<original.length;i++){
-//            copy[i]=original[i];
-//        }//
-//        return copy;
-//    }//
-//    public static Collection<?> deepCopy1(Collection<?> original){
-//        Collection<?> copy=;
-//        return copy;
-//    }
+    public static <E> E deepCopy(E original) {
+        E[] decon = (E[]) original;
+        E[] copy = decon.clone();
+        if (decon[0].getClass().isArray()) {
+            for (int i = 0; i < decon.length; i++) {
+                copy[i] = deepCopy(decon[i]);
+            }//for(int i=0;i<decon.length;i++){
+        }//if(decon[0].getClass().isArray()){
+        else {
+            copy = decon.clone();
+        }//else{
+        E recon = (E) copy;
+        return recon;
+    }//public static <E> E deepCopyTest (E original){
 }//public class Helpers{
