c0e68d788166eff249a2963b54b0ba0b08116d67
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-16 16:16:55 -0500

    interview_practice

--- src UnitTests/interview_practice/_0_Data_Structures/FirstDuplicateTest.java
+++ src UnitTests/interview_practice/_0_Data_Structures/FirstDuplicateTest.java
@@ -1,4 +1,105 @@
-import static org.junit.jupiter.api.Assertions.*;
+/*
+ * Authored by: Jason Wesley Howse
+ */
+
+package interview_practice._0_Data_Structures;
+
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
 class FirstDuplicateTest {
-  
-}
+    @Test
+    void test1() {
+        int[] testCase = {2, 1, 3, 5, 3, 2};
+        int solution =3;
+        test(testCase, solution);
+    }//void test1() {
+
+    @Test
+    void test2() {
+        int[] testCase = {2,2};
+        int solution =2;
+        test(testCase, solution);
+    }//void test2() {
+
+    @Test
+    void test3() {
+        int[] testCase = {2, 4, 3, 5, 1};
+        int solution =-1;
+        test(testCase, solution);
+    }//void test3() {
+
+    @Test
+    void test4() {
+        int[] testCase = {1};
+        int solution =-1;
+        test(testCase, solution);
+    }//void test4() {
+
+    @Test
+    void test5() {
+        int[] testCase = {5, 5, 5, 5, 5};
+        int solution =5;
+        test(testCase, solution);
+    }//void test5() {
+
+    @Test
+    void test6() {
+        int[] testCase = {2, 1};
+        int solution =-1;
+        test(testCase, solution);
+    }//void test6() {
+
+    @Test
+    void test7() {
+        int[] testCase = {2, 1, 3};
+        int solution =-1;
+        test(testCase, solution);
+    }//void test7() {
+
+    @Test
+    void test8() {
+        int[] testCase = {2, 3, 3};
+        int solution =3;
+        test(testCase, solution);
+    }//void test8() {
+
+    @Test
+    void test9() {
+        int[] testCase = {3, 3, 3};
+        int solution =3;
+        test(testCase, solution);
+    }//void test9() {
+
+    @Test
+    void test10() {
+        int[] testCase = {8, 4, 6, 2, 6, 4, 7, 9, 5, 8};
+        int solution =6;
+        test(testCase, solution);
+    }//void test10() {
+
+    @Test
+    void test11() {
+        int[] testCase = {10, 6, 8, 4, 9, 1, 7, 2, 5, 3};
+        int solution =-1;
+        test(testCase, solution);
+    }//void test11() {
+
+    @Test
+    void test12() {
+        int[] testCase = {1, 1, 2, 2, 1};
+        int solution =1;
+        test(testCase, solution);
+    }//void test12() {
+
+
+    void test(int []testCase,int solution){
+        assertTimeout(Duration.ofSeconds(3),()->
+                new FirstDuplicate().firstDuplicate(testCase.clone()));
+        assertEquals(new FirstDuplicate().firstDuplicate(testCase),solution);
+
+    }
+
+}//class FirstDuplicateTest {
--- src/interview_practice/_0_Data_Structures/FirstDuplicate.java
+++ src/interview_practice/_0_Data_Structures/FirstDuplicate.java
@@ -1,14 +1,41 @@
-package interview_practice._0_Data_Structures;public class FirstDuplicate {
-
 /*
+ * Authored by: Jason Wesley Howse
+ */
 
-*/
+package interview_practice._0_Data_Structures;
+import java.util.HashSet;
 
-//todo: finish class
+public class FirstDuplicate {
 
+    /*
+Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+   */
 
-/*
+    int firstDuplicate(int[] a){
+        HashSet<Integer> b=new HashSet<>();
+        for (int j : a) {
+            if (b.contains(j)) {
+                return j;
+            }//if (b.contains(j)) {
+            b.add(j);
+        }//for (int j : a) {
+        return -1;
+    }//int firstDuplicate(int[] a){
 
-*/
+    /*
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] array.integer a
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 105,
+1 ≤ a[i] ≤ a.length.
+
+[output] integer
+
+The element in a that occurs in the array more than once and has the minimal index for its second occurrence. If there are no such elements, return -1.
+     */
 
 }//public class FirstDuplicate {
