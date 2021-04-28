7e23a7918778659184e96511a5afe5e7667a7628
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-28 11:50:18 -0500

    Helper functions update

---
+++ src/helper_functions/ListNode.cs
@@ -0,0 +1,6 @@
+﻿namespace Algorithms.src.helper_functions {
+    public class ListNode<T> {
+        public T value { get; set; }
+        public ListNode<T> next { get; set; }
+    }//class ListNode<T> {
+}//namespace Algorithms.src.helper_functions {
