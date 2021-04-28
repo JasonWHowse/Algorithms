13a69e0e3f2674bafd1111c0b747891b645c72f7
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-28 09:51:22 -0500

    IsSumOfConsecutive2.cs

---
+++ src UnitTests/_1_the_core/_05_labyrinth_of_nested_loops/IsSumOfConsecutive2Tests.cs
@@ -0,0 +1,63 @@
+﻿using Algorithms.src._1_the_core._05_labyrinth_of_nested_loops;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._05_labyrinth_of_nested_loops {
+    [TestClass()]
+    public class IsSumOfConsecutive2Tests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = 9;
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = 8;
+            var solution = 0;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = 15;
+            var solution = 3;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = 24;
+            var solution = 1;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = 13;
+            var solution = 1;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = 25;
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase = 99;
+            var solution = 5;
+            Test(testCase, solution);
+        }//public void Test7() {
+
+        private void Test(int testCase, int solution) {
+            Assert.AreEqual(solution, new IsSumOfConsecutive2().isSumOfConsecutive2(testCase));
+        }//private void Test(int testCase,int solution) {
+    }//public class IsSumOfConsecutive2Tests {
+}//namespace Algorithms.src_UnitTests._1_the_core._05_labyrinth_of_nested_loops {
---
+++ src/_1_the_core/_05_labyrinth_of_nested_loops/IsSumOfConsecutive2.cs
@@ -0,0 +1,46 @@
+﻿namespace Algorithms.src._1_the_core._05_labyrinth_of_nested_loops {
+    public class IsSumOfConsecutive2 {
+
+        /*
+
+Find the number of ways to express n as sum of some (at least two) consecutive positive integers.
+
+Example
+
+For n = 9, the output should be
+isSumOfConsecutive2(n) = 2.
+
+There are two ways to represent n = 9: 2 + 3 + 4 = 9 and 4 + 5 = 9.
+
+For n = 8, the output should be
+isSumOfConsecutive2(n) = 0.
+
+There are no ways to represent n = 8.
+
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] integer n
+
+A positive integer.
+
+Guaranteed constraints:
+1 ≤ n ≤ 104.
+
+[output] integer
+
+        */
+
+        public
+        int isSumOfConsecutive2(int n) {
+            int output= n % 2;
+            for (int i = 1; i <= n / 2; i += 2) {
+                if (n % i == 0) {
+                output++;
+                }//if (n % i == 0) {
+            }//for (int i = 1; i <= n / 2; i += 2) {
+            return --output;
+        }//int isSumOfConsecutive2(int n) {
+    }//public class IsSumOfConsecutive2 {
+}//namespace Algorithms.src._1_the_core._05_labyrinth_of_nested_loops {
