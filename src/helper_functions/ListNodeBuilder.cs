76fd7c19cbb2213f7fe4f08212c30a761055dbb6
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-28 11:50:39 -0500

    Helper functions update

---
+++ src/helper_functions/ListNodeBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithms.src.helper_functions {
+    public static class ListNodeBuilder {
+        public static ListNode<T> CollectionToListNode<T>(T[] values) {
+            if (values.Length == 0) {
+                return null;
+            }//if (values.Length == 0) {
+            ListNode<T> Head = new ListNode<T>();
+            ListNode<T> Output = Head;
+            for(int i=0;i<values.Length;){
+                Head.value = values[i];
+                if (++i < values.Length) {
+                    Head.next = new ListNode<T>();
+                    Head = Head.next;
+                }//if (++i < values.Length) {
+            }//for(int i=0;i<values.Length;){
+            return Output;
+        }//public static ListNode<T> CollectionToListNode<T>(T[] values) {
+    }//public static class ListNodeBuilder {
+}//namespace Algorithms.src.helper_functions {
