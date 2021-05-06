2dc560d6594a6fd3af6c6c1e6ad17b6ed02ac901
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-05-05 20:06:37 -0500

    SquareDigitsSequence.cs

---
+++ src UnitTests/_1_the_core/_05_labyrinth_of_nested_loops/SquareDigitsSequenceTests.cs
@@ -0,0 +1,56 @@
+﻿using Algorithms.src._1_the_core._05_labyrinth_of_nested_loops;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._05_labyrinth_of_nested_loops {
+    [TestClass()]
+    public class SquareDigitsSequenceTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = 16;
+            var solution = 9;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = 103;
+            var solution = 4;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = 1;
+            var solution = 2;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = 13;
+            var solution = 4;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = 89;
+            var solution = 9;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = 612;
+            var solution = 16;
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        private void Test(int testCase, int solution) {
+            Assert.AreEqual(solution, new SquareDigitsSequence().squareDigitsSequence(testCase));
+        }//private void Test(int testCase,int solution) {
+    }//public class SquareDigitsSequenceTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._05_labyrinth_of_nested_loops {
