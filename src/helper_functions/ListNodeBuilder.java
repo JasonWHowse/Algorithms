fdf3da23d92ba9d8ae1faa849091d7258d6fdde9
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-05-16 16:36:55 -0500

    helper_functions update

--- src/helper_functions/ListNodeBuilder.java
+++ src/helper_functions/ListNodeBuilder.java
@@ -20,4 +20,20 @@ public class ListNodeBuilder {
         }//for(int i=0;i<values.length;){
         return Output;
     }//public static <T> ListNode<T> CollectionToListNode(T[] values) {
+
+    public static <T> ListNode<T> ListNodeClone(ListNode<T> Original){
+        if(Original==null){
+            return null;
+        }//if(Original==null){
+        ListNode<T> CopyOutput = new ListNode<>(Original.value);
+        ListNode<T> Copy = CopyOutput;
+        while(Original!=null){
+            if(Original.next!=null){
+                Copy.next=new ListNode<>(Original.next.value);
+                Copy=Copy.next;
+            }//if(Original.next!=null){
+            Original=Original.next;
+        }//while(Original!=null){
+        return CopyOutput;
+    }//private ListNode<Integer> ListNodeClone(ListNode<Integer> Original){
 }//public class ListNodeBuilder<T> {
