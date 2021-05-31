aa121ead2ed002911f7934a428bfe222f586803f
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-05-30 22:34:36 -0500

    Updates + Helpers.cs Json testing compatibility

---
+++ src/helper_functions/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Algorithms.src.helper_functions {
+    class Helpers {
+        public static JObject GetJsonObject(string fileName, string path) {
+            JObject output = null;
+            try {
+                output = (JObject)JsonConvert.DeserializeObject(new StreamReader(Path.GetFullPath("Algorithms.iml").Split("bin\\Debug")[0] + path + fileName).ReadToEnd());
+            } catch (Exception) { }//try {
+            return output;
+        }//public static JObject GetJsonObject(string fileName, string path) {
+    }//class Helpers {
+}//namespace Algorithms.src.helper_functions {
