e9ee7861b2f0bb69bbd3216bb36b06b6649e6f11
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-05-30 22:36:12 -0500

    PossibleSums.cs

---
+++ src UnitTests/interview_practice/_0_Data_Structures/_2_Hash_Tables/PossibleSumsTests.cs
@@ -0,0 +1,142 @@
+﻿using Algorithms.src.interview_practice._0_Data_Structures._2_Hash_Tables;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests.interview_practice._0_Data_Structures._2_Hash_Tables {
+    [TestClass()]
+    public class PossibleSumsTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            int[] testCase1 = { 10, 50, 100 };
+            int[] testCase2 = { 1, 2, 1 };
+            var solution = 9;
+            Test(testCase1, testCase2, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            int[] testCase1 = { 10, 50, 100, 500 };
+            int[] testCase2 = { 5, 3, 2, 2 };
+            var solution = 122;
+            Test(testCase1, testCase2, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            int[] testCase1 = { 1 };
+            int[] testCase2 = { 5 };
+            var solution = 5;
+            Test(testCase1, testCase2, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            int[] testCase1 = { 1, 1 };
+            int[] testCase2 = { 2, 3 };
+            var solution = 5;
+            Test(testCase1, testCase2, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            int[] testCase1 = { 1, 2 };
+            int[] testCase2 = { 50000, 2 };
+            var solution = 50004;
+            Test(testCase1, testCase2, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            int[] testCase1 = { 1, 2, 3 };
+            int[] testCase2 = { 2, 3, 10000 };
+            var solution = 30008;
+            Test(testCase1, testCase2, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            int[] testCase1 = { 3, 1, 1 };
+            int[] testCase2 = { 111, 84, 104 };
+            var solution = 521;
+            Test(testCase1, testCase2, solution);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            int[] testCase1 = { 1, 1, 1, 1, 1 };
+            int[] testCase2 = { 9, 19, 18, 12, 19 };
+            var solution = 77;
+            Test(testCase1, testCase2, solution);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            int[] testCase1 = { 3, 2, 5, 2, 3, 5, 4 };
+            int[] testCase2 = { 4, 6, 4, 9, 6, 8, 8 };
+            var solution = 150;
+            Test(testCase1, testCase2, solution);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            int[] testCase1 = { 6, 12, 7, 16, 8, 5, 17, 18, 6 };
+            int[] testCase2 = { 3, 4, 4, 2, 6, 4, 4, 2, 5 };
+            var solution = 318;
+            Test(testCase1, testCase2, solution);
+        }//public void Test10() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test11() {
+            int[] testCase1 = { 13, 8, 31, 7, 10, 36, 28, 11, 11, 11 };
+            int[] testCase2 = { 6, 3, 2, 2, 2, 2, 6, 1, 5, 4 };
+            var solution = 532;
+            Test(testCase1, testCase2, solution);
+        }//public void Test11() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test12() {
+            int[] testCase1 = { 34, 39, 20, 47, 33, 49, 53, 8, 48, 44, 51, 47 };
+            int[] testCase2 = { 2, 3, 2, 2, 2, 4, 1, 2, 3, 3, 2, 1 };
+            var solution = 1005;
+            Test(testCase1, testCase2, solution);
+        }//public void Test12() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test13() {
+            int[] testCase1 = { 50, 48, 44, 46, 34, 53, 29, 27, 38, 30, 54, 56, 6, 55, 31 };
+            int[] testCase2 = { 3, 1, 1, 2, 3, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2 };
+            var solution = 902;
+            Test(testCase1, testCase2, solution);
+        }//public void Test13() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test14() {
+            int[] testCase1 = { 50, 48, 44, 46, 34, 53, 29, 27, 38, 30, 54, 56, 6, 55, 31 };
+            int[] testCase2 = { 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1 };
+            var solution = 862;
+            Test(testCase1, testCase2, solution);
+        }//public void Test14() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test15() {
+            int[] testCase1 = { 5, 46, 27, 17, 54, 10, 14, 54, 40, 44, 22, 61, 19, 20, 68, 40, 2, 31 };
+            int[] testCase2 = { 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2 };
+            var solution = 651;
+            Test(testCase1, testCase2, solution);
+        }//public void Test15() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test16() {
+            int[] testCase1 = { 17, 27, 22, 18, 9, 58, 50, 85, 67, 50, 42, 11, 60, 39, 23, 37, 28, 91, 81 };
+            int[] testCase2 = { 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            var solution = 862;
+            Test(testCase1, testCase2, solution);
+        }//public void Test16() {
+
+        private void Test(int[] testCase1, int[] testCase2, int solution) {
+            Assert.AreEqual(solution, new PossibleSums().possibleSums(testCase1, testCase2));
+        }//private void Test(int[] testCase1, int[] testCase2, int solution) {
+    }//public class PossibleSumsTests {
+}//namespace Algorithms.src_UnitTests.interview_practice._0_Data_Structures._2_Hash_Tables {
---
+++ src/interview_practice/_0_Data_Structures/_2_Hash_Tables/PossibleSums.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.src.interview_practice._0_Data_Structures._2_Hash_Tables {
+    public class PossibleSums {
+
+        /*
+
+You have a collection of coins, and you know the values of the coins and the quantity of each type of coin in it. You want to know how many distinct sums you can make from non-empty groupings of these coins.
+
+Example
+
+For coins = [10, 50, 100] and quantity = [1, 2, 1], the output should be
+possibleSums(coins, quantity) = 9.
+
+Here are all the possible sums:
+
+50 = 50;
+10 + 50 = 60;
+50 + 100 = 150;
+10 + 50 + 100 = 160;
+50 + 50 = 100;
+10 + 50 + 50 = 110;
+50 + 50 + 100 = 200;
+10 + 50 + 50 + 100 = 210;
+10 = 10;
+100 = 100;
+10 + 100 = 110.
+As you can see, there are 9 distinct sums that can be created from non-empty groupings of your coins.
+
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] array.integer coins
+
+An array containing the values of the coins in your collection.
+
+Guaranteed constraints:
+1 ≤ coins.length ≤ 20,
+1 ≤ coins[i] ≤ 104.
+
+[input] array.integer quantity
+
+An array containing the quantity of each type of coin in your collection. quantity[i] indicates the number of coins that have a value of coins[i].
+
+Guaranteed constraints:
+quantity.length = coins.length,
+1 ≤ quantity[i] ≤ 105,
+(quantity[0] + 1) * (quantity[1] + 1) * ... * (quantity[quantity.length - 1] + 1) <= 106.
+
+[output] integer
+
+The number of different possible sums that can be created from non-empty groupings of your coins.
+
+        */
+
+        public
+        int possibleSums(int[] coins, int[] quantity) {
+            HashSet<int> output = new HashSet<int>();
+            output.Add(0);
+            for(int i = 0; i < coins.Length; i++) {
+                List<int> list = new List<int>(output);
+                foreach(int o in list) {
+                    for(int j=0; j <= quantity[i]; j++) {
+                        output.Add(o + j * coins[i]);
+                    }//for(int j=0; j <= quantity[i]; j++) {
+                }//foreach(int o in list) {
+            }//for(int i = 0; i < coins.Length; i++) {
+            return output.Count-1;
+        }//int possibleSums(int[] coins, int[] quantity) {
+    }//public class PossibleSums {
+}//namespace Algorithms.src.interview_practice._0_Data_Structures._2_Hash_Tables {
