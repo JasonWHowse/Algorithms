cc28434e0afd62b525976277abf53311e3d6c211
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-02 08:45:11 -0500

    _02_corner_of_0s_and_1s Tests update, adding new tests and adding json file compatibility for Java

--- src UnitTests/_1_the_core/_02_corner_of_0s_and_1s/ArrayPackingTest.java
+++ src UnitTests/_1_the_core/_02_corner_of_0s_and_1s/ArrayPackingTest.java
@@ -5,52 +5,84 @@
 package _1_the_core._02_corner_of_0s_and_1s;
 
 import org.junit.jupiter.api.Test;
-
 import java.time.Duration;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class ArrayPackingTest {
 
     @Test
-    void test1() {
-        int[] testCase= {24, 85, 0};
-        int solution=21784;
-        test(testCase,solution);
-    }//void test1() {
+    public void test1() {
+        int[] testCase = { 24, 85, 0 };
+        var solution = 21784;
+        test(testCase, solution);
+    }//public void test1() {
 
     @Test
-    void test2() {
-        int[] testCase= {23, 45, 39};
-        int solution=2567447;
-        test(testCase,solution);
-    }//void test2() {
+    public void test2() {
+        int[] testCase = { 23, 45, 39 };
+        var solution = 2567447;
+        test(testCase, solution);
+    }//public void test2() {
 
     @Test
-    void test3() {
-        int[] testCase= {1, 2, 4, 8};
-        int solution=134480385;
-        test(testCase,solution);
-    }//void test3() {
+    public void test3() {
+        int[] testCase = { 1, 2, 4, 8 };
+        var solution = 134480385;
+        test(testCase, solution);
+    }//public void test3() {
 
     @Test
-    void test4() {
-        int[] testCase= {5};
-        int solution=5;
-        test(testCase,solution);
-    }//void test4() {
+    public void test4() {
+        int[] testCase = { 5 };
+        var solution = 5;
+        test(testCase, solution);
+    }//public void test4() {
 
     @Test
-    void test5() {
-        int[] testCase= {187, 99, 42, 43};
-        int solution=724198331;
-        test(testCase,solution);
-    }//void test5() {
+    public void test5() {
+        int[] testCase = { 187, 99, 42, 43 };
+        var solution = 724198331;
+        test(testCase, solution);
+    }//public void test5() {
+
+    @Test
+    public void test6() {
+        int[] testCase = { 0 };
+        var solution = 0;
+        test(testCase, solution);
+    }//public void test6() {
+
+    @Test
+    public void test7() {
+        int[] testCase = { 207, 78, 212, 28 };
+        var solution = 483675855;
+        test(testCase, solution);
+    }//public void test7() {
+
+    @Test
+    public void test8() {
+        int[] testCase = { 111, 8, 150 };
+        var solution = 9832559;
+        test(testCase, solution);
+    }//public void test8() {
+
+    @Test
+    public void test9() {
+        int[] testCase = { 242, 30 };
+        var solution = 7922;
+        test(testCase, solution);
+    }//public void test9() {
+
+    @Test
+    public void test10() {
+        int[] testCase = { 255, 255, 255, 127 };
+        var solution = 2147483647;
+        test(testCase, solution);
+    }//public void test10() {
     
     private void test(int[] testCase,int solution){
-        assertTimeout(Duration.ofSeconds(3),()->
-                new ArrayPacking().arrayPacking(testCase.clone()));
+        assertTimeout(Duration.ofSeconds(3),()-> new ArrayPacking().arrayPacking(testCase.clone()));
         assertEquals(solution,new ArrayPacking().arrayPacking(testCase.clone()));
     }//private void test(int[] testCase,int solution){
 }//class ArrayPackingTest {
