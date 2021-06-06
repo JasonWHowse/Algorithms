28c4530a2d4d99a2b216f1e659df3b368246b7fd
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-06 17:51:33 -0500

    update

--- src UnitTests/interview_practice/_3_Special_Topics/_0_Common_Techniques_Basic/ContainsDuplicatesTest.java
+++ src UnitTests/interview_practice/_3_Special_Topics/_0_Common_Techniques_Basic/ContainsDuplicatesTest.java
@@ -20,121 +20,121 @@ class ContainsDuplicatesTest {
     public void test1(){
         int[] testCase = { 1, 2, 3, 1 };
         test(testCase, true);
-    }//public void Test1(){
+    }//public void test1(){
 
     @Test
     public void test2(){
         int[] testCase = { 3, 1 };
         test(testCase, false);
-    }//public void Test2(){
+    }//public void test2(){
 
     @Test
     public void test3(){
         int[] testCase = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
         test(testCase, true);
-    }//public void Test3(){
+    }//public void test3(){
 
     @Test
     public void test4(){
         int[] testCase = { };
         test(testCase, false);
-    }//public void Test4(){
+    }//public void test4(){
 
     @Test
     public void test5(){
         int[] testCase = { 1 };
         test(testCase, false);
-    }//public void Test5(){
+    }//public void test5(){
 
     @Test
     public void test6(){
         int[] testCase = { -1200000005, -1200000005 };
         test(testCase, true);
-    }//public void Test6(){
+    }//public void test6(){
 
     @Test
     public void test7(){
         int[] testCase = { 0, 4, 5, 0, 3, 6 };
         test(testCase, true);
-    }//public void Test7(){
+    }//public void test7(){
 
     @Test
     public void test8(){
         int[] testCase = { 1, 2, 3, 4 };
         test(testCase, false);
-    }//public void Test8(){
+    }//public void test8(){
 
     @Test
     public void test9(){
         int[] testCase = { 0, 1, 0, -1 };
         test(testCase, true);
-    }//public void Test9(){
+    }//public void test9(){
 
     @Test
     public void test10(){
         int[] testCase = { 2 };
         test(testCase, false);
-    }//public void Test10(){
+    }//public void test10(){
 
     @Test
     public void test11(){
         int[] testCase = { 0 };
         test(testCase, false);
-    }//public void Test11(){
+    }//public void test11(){
 
     @Test
     public void test12(){
         int[] testCase = { 1, 5, -2, -4, 0 };
         test(testCase, false);
-    }//public void Test12(){
+    }//public void test12(){
 
     @Test
     public void test13(){
         int[] testCase = { 2, 14, 18, 22, 22 };
         test(testCase, true);
-    }//public void Test13(){
+    }//public void test13(){
 
     @Test
     public void test14(){
         int[] testCase = { 3, 3 };
         test(testCase, true);
-    }//public void Test14(){
+    }//public void test14(){
 
     @Test
     public void test15(){
         setTestCaseInfo("test-15.json");
         test(internedTestCase, internedSolution);
-    }//public void Test15(){
+    }//public void test15(){
 
     @Test
     public void test16(){
         int[] testCase = { 7, 10, 5, 5, 6, 6, 4, 10, 5, 4, 9, 4, 9, 6, 5, 9, 6, 3, 6, 5, 6, 7, 7, 4, 9, 9, 10, 5, 8, 1, 8, 3, 2, 7, 5, 10, 1, 8, 5, 8, 4, 3, 6, 4, 9, 4, 2, 8, 3, 2, 2, 1, 5, 6, 3, 2, 6, 1, 8, 6, 2, 9, 1, 4, 5, 10, 8, 5, 10, 5, 10, 1, 4, 8, 3, 6, 4, 10, 9, 1, 1, 1, 2, 2, 9, 6, 6, 8, 1, 9, 2, 5, 5, 2, 1, 8, 5, 2, 3, 10 };
         test(testCase, true);
-    }//public void Test16(){
+    }//public void test16(){
 
     @Test
     public void test17(){
         setTestCaseInfo("test-17.json");
         test(internedTestCase, internedSolution);
-    }//public void Test17(){
+    }//public void test17(){
 
     @Test
     public void test18(){
         setTestCaseInfo("test-18.json");
         test(internedTestCase, internedSolution);
-    }//public void Test18(){
+    }//public void test18(){
 
     @Test
     public void test19(){
         setTestCaseInfo("test-19.json");
         test(internedTestCase, internedSolution);
-    }//public void Test19(){
+    }//public void test19(){
 
     @Test
     public void test20(){
         setTestCaseInfo("test-20.json");
         test(internedTestCase, internedSolution);
-    }//public void Test20(){
+    }//public void test20(){
 
     private void test(int[] testCase, boolean solution){
         assertTimeout(Duration.ofSeconds(3),()->new ContainsDuplicates().containsDuplicates(testCase.clone()));
