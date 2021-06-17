02e1ff970e2ba3e21fed0d3289a9d4f652690b29
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-17 11:03:22 -0500

    IsTandemRepeat.cs

---
+++ src UnitTests/_1_the_core/_06_boom_market/IsTandemRepeatTests.cs
@@ -0,0 +1,84 @@
+﻿using Algorithms.src._1_the_core._06_boom_market;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._06_boom_market {
+    [TestClass()]
+    public class IsTandemRepeatTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = "tandemtandem";
+            var solution = true;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = "qqq";
+            var solution = false;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = "2w2ww";
+            var solution = false;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = "hophey";
+            var solution = false;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = "CodeSignalCodeSignal";
+            var solution = true;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = "interestinterest";
+            var solution = true;
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase = "aa";
+            var solution = true;
+            Test(testCase, solution);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase = "ab";
+            var solution = false;
+            Test(testCase, solution);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase = "stringString";
+            var solution = false;
+            Test(testCase, solution);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase = "truetruetrue";
+            var solution = false;
+            Test(testCase, solution);
+        }//public void Test10() {
+
+        private void Test(string testCase, bool solution) {
+            Assert.AreEqual(solution, new IsTandemRepeat().isTandemRepeat(testCase));
+        }//private void Test(string testCase,string solution) {
+    }//public class IsTandemRepeatTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._06_boom_market {
---
+++ src/_1_the_core/_06_boom_market/IsTandemRepeat.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.src._1_the_core._06_boom_market {
+    public class IsTandemRepeat {
+
+        /*
+
+Determine whether the given string can be obtained by one concatenation of some string to itself.
+
+Example
+
+For inputString = "tandemtandem", the output should be
+isTandemRepeat(inputString) = true;
+For inputString = "qqq", the output should be
+isTandemRepeat(inputString) = false;
+For inputString = "2w2ww", the output should be
+isTandemRepeat(inputString) = false.
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string inputString
+
+Guaranteed constraints:
+2 ≤ inputString.length ≤ 20.
+
+[output] boolean
+
+true if inputString represents a string concatenated to itself, false otherwise.
+
+        */
+
+        public
+        bool isTandemRepeat(string inputString) {
+            return inputString.Equals(inputString[(inputString.Length / 2)..] + inputString[(inputString.Length / 2)..]);
+        }//bool isTandemRepeat(string inputString) {
+    }//public class IsTandemRepeat {
+}//namespace Algorithms.src._1_the_core._06_boom_market {
