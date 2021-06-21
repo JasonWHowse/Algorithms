eed6d57b040cda0197496aaada34e804ff3446cc
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-21 11:16:07 -0500

    HtmlEndTagByStartTag.cs

---
+++ src UnitTests/_1_the_core/_06_book_market/HtmlEndTagByStartTagTests.cs
@@ -0,0 +1,56 @@
+﻿using Algorithms.src._1_the_core._06_book_market;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._06_book_market {
+    [TestClass()]
+    public class HtmlEndTagByStartTagTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = "<button type='button' disabled>";
+            var solution = "</button>";
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = "<i>";
+            var solution = "</i>";
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = "<div id='my_area' class='data' title='This is a test for title on Div tag'>";
+            var solution = "</div>";
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = "<html>";
+            var solution = "</html>";
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = "<TABLE border='1'>";
+            var solution = "</TABLE>";
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = "<li class='test'>";
+            var solution = "</li>";
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        private void Test(string testCase, string solution) {
+            Assert.AreEqual(solution, new HtmlEndTagByStartTag().htmlEndTagByStartTag(testCase));
+        }//private void Test(string testCase,string solution) {
+    }//public class HtmlEndTagByStartTagTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._06_book_market {
---
+++ src/_1_the_core/_06_book_market/HtmlEndTagByStartTag.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.src._1_the_core._06_book_market {
+    public class HtmlEndTagByStartTag {
+
+        /*
+
+You are implementing your own HTML editor. To make it more comfortable for developers you would like to add an auto-completion feature to it.
+
+Given the starting HTML tag, find the appropriate end tag which your editor should propose.
+
+If you are not familiar with HTML, consult with this note.
+
+Example
+
+For startTag = "<button type='button' disabled>", the output should be
+htmlEndTagByStartTag(startTag) = "</button>";
+For startTag = "<i>", the output should be
+htmlEndTagByStartTag(startTag) = "</i>".
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string startTag
+
+Guaranteed constraints:
+3 ≤ startTag.length ≤ 75.
+
+[output] string
+
+        */
+
+        public
+        string htmlEndTagByStartTag(string startTag) {
+            return "</"+startTag.Remove(startTag.IndexOf(" ")>0? startTag.IndexOf(" ") : startTag.IndexOf(">"))[1..]+">";
+        }//string htmlEndTagByStartTag(string startTag) {
+    }//public class HtmlEndTagByStartTag {
+}//namespace Algorithms.src._1_the_core._06_book_market {
