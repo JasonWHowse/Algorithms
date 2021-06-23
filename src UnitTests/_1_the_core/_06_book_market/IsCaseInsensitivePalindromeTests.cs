47cee53dbee1290f4bac32d34d255439ae73166c
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-23 09:43:19 -0500

    IsCaseInsensitivePalindrome.cs

---
+++ src UnitTests/_1_the_core/_06_book_market/IsCaseInsensitivePalindromeTests.cs
@@ -0,0 +1,78 @@
+﻿using Algorithms.src._1_the_core._06_book_market;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._06_book_market {
+    [TestClass()]
+    public class IsCaseInsensitivePalindromeTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = "AaBaa";
+            Test(testCase, true);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = "abac";
+            Test(testCase, false);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = "aBACaba";
+            Test(testCase, true);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = "opOP";
+            Test(testCase, false);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = "ZZzzazZzz";
+            Test(testCase, true);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = "zzzzazzzz";
+            Test(testCase, true);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase = "ZZzzabzZzz";
+            Test(testCase, false);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase = "abcdcbA";
+            Test(testCase, true);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase = "abracabra";
+            Test(testCase, false);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase = "abcd";
+            Test(testCase, false);
+        }//public void Test10() {
+
+        private void Test(string testCase, bool solution) {
+            if (solution) {
+                Assert.IsTrue(new IsCaseInsensitivePalindrome().isCaseInsensitivePalindrome(testCase));
+            } else {//if (solution) {
+                Assert.IsFalse(new IsCaseInsensitivePalindrome().isCaseInsensitivePalindrome(testCase));
+            }//else {
+        }//private void Test(string testCase,bool solution) {
+    }//public class IsCaseInsensitivePalindromeTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._06_book_market {
---
+++ src/_1_the_core/_06_book_market/IsCaseInsensitivePalindrome.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Algorithms.src._1_the_core._06_book_market {
+    public class IsCaseInsensitivePalindrome {
+
+        /*
+
+Given a string, check if it can become a palindrome through a case change of some (possibly, none) letters.
+
+Example
+
+For inputString = "AaBaa", the output should be
+isCaseInsensitivePalindrome(inputString) = true.
+
+"aabaa" is a palindrome as well as "AABAA", "aaBaa", etc.
+
+For inputString = "abac", the output should be
+isCaseInsensitivePalindrome(inputString) = false.
+
+All the strings which can be obtained via changing case of some group of letters, i.e. "abac", "Abac", "aBAc" and 13 more, are not palindromes.
+
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string inputString
+
+Non-empty string consisting of English letters.
+
+Guaranteed constraints:
+4 ≤ inputString.length ≤ 10.
+
+[output] boolean
+
+        */
+
+        public
+        bool isCaseInsensitivePalindrome(string inputString) {
+            return inputString.ToLower().Equals(new string(inputString.ToCharArray().Reverse().ToArray()).ToLower());
+        }//bool isCaseInsensitivePalindrome(string inputString) {
+    }//public class IsCaseInsensitivePalindrome {
+}//namespace Algorithms.src._1_the_core._06_book_market {
