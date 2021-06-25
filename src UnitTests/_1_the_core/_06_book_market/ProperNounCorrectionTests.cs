8fe6b546f64d8dc86c0237a6f54ccfe0cc39d4cf
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-25 08:45:17 -0500

    ProperNounCorrection.cs

---
+++ src UnitTests/_1_the_core/_06_book_market/ProperNounCorrectionTests.cs
@@ -0,0 +1,84 @@
+﻿using Algorithms.src._1_the_core._06_book_market;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._06_book_market {
+    [TestClass()]
+    public class ProperNounCorrectionTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = "pARiS";
+            var solution = "Paris";
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = "John";
+            var solution = "John";
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = "mary";
+            var solution = "Mary";
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = "a";
+            var solution = "A";
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = "B";
+            var solution = "B";
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = "yFZMlGvUQP";
+            var solution = "Yfzmlgvuqp";
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase = "HQQrrDxuqe";
+            var solution = "Hqqrrdxuqe";
+            Test(testCase, solution);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase = "atDh";
+            var solution = "Atdh";
+            Test(testCase, solution);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase = "pJHSYVAZB";
+            var solution = "Pjhsyvazb";
+            Test(testCase, solution);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase = "dKrqO";
+            var solution = "Dkrqo";
+            Test(testCase, solution);
+        }//public void Test10() {
+
+        private void Test(string testCase, string solution) {
+            Assert.AreEqual(solution, new ProperNounCorrection().properNounCorrection(testCase));
+        }//private void Test(string testCase,string solution) {
+    }//public class ProperNounCorrectionTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._06_book_market {
---
+++ src/_1_the_core/_06_book_market/ProperNounCorrection.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithms.src._1_the_core._06_book_market {
+    public class ProperNounCorrection {
+
+        /*
+
+Proper nouns always begin with a capital letter, followed by small letters.
+
+Correct a given proper noun so that it fits this statement.
+
+Example
+
+For noun = "pARiS", the output should be
+properNounCorrection(noun) = "Paris";
+For noun = "John", the output should be
+properNounCorrection(noun) = "John".
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string noun
+
+A string representing a proper noun with a mix of capital and small English letters.
+
+Guaranteed constraints:
+1 ≤ noun.length ≤ 10.
+
+[output] string
+
+Corrected (if needed) noun.
+
+        */
+
+        public
+        string properNounCorrection(string noun) {
+            return noun[0].ToString().ToUpper() + noun[1..].ToLower();
+        }//string properNounCorrection(string noun) {
+    }//public class ProperNounCorrection {
+}//namespace Algorithms.src._1_the_core._06_book_market {
