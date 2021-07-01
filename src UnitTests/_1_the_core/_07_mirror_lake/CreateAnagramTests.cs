d7ce0d2f114022227d59daadb1cf1e55fb4ceb61
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-07-01 08:55:34 -0500

    CreateAnagram.cs

---
+++ src UnitTests/_1_the_core/_07_mirror_lake/CreateAnagramTests.cs
@@ -0,0 +1,94 @@
+﻿using Algorithms.src._1_the_core._07_mirror_lake;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
+    [TestClass()]
+    public class CreateAnagramTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase1 = "AABAA";
+            var testCase2 = "BBAAA";
+            var solution = 1;
+            Test(testCase1, testCase2, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase1 = "OVGHK";
+            var testCase2 = "RPGUC";
+            var solution = 4;
+            Test(testCase1, testCase2, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB";
+            var testCase2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC";
+            var solution = 1;
+            Test(testCase1, testCase2, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase1 = "HDFFVR";
+            var testCase2 = "FEDDEE";
+            var solution = 4;
+            Test(testCase1, testCase2, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase1 = "AABCDS";
+            var testCase2 = "BASEAE";
+            var solution = 2;
+            Test(testCase1, testCase2, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase1 = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZY";
+            var testCase2 = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYZ";
+            var solution = 31;
+            Test(testCase1, testCase2, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            var testCase2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase1 = "AAAAAA";
+            var testCase2 = "AAAAAA";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase1 = "KJDMDLEEKALIJB";
+            var testCase2 = "AFDJGDCGHMIGHB";
+            var solution = 7;
+            Test(testCase1, testCase2, solution);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase1 = "BBAAABCBCAABB";
+            var testCase2 = "BBBCCCBABBACA";
+            var solution = 2;
+            Test(testCase1, testCase2, solution);
+        }//public void Test10() {
+
+        private void Test(string testCase1, string testCase2, int solution) {
+            Assert.AreEqual(solution, new CreateAnagram().createAnagram(testCase1, testCase2));
+        }//private void Test(string testCase1, string testCase2, int solution) {
+    }//public class CreateAnagramTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
---
+++ src/_1_the_core/_07_mirror_lake/CreateAnagram.cs
@@ -0,0 +1,44 @@
+﻿namespace Algorithms.src._1_the_core._07_mirror_lake {
+    public class CreateAnagram {
+
+        /*
+
+You are given two strings s and t of the same length, consisting of uppercase English letters. Your task is to find the minimum number of "replacement operations" needed to get some anagram of the string t from the string s. A replacement operation is performed by picking exactly one character from the string s and replacing it by some other character.
+
+Example
+
+For s = "AABAA" and t = "BBAAA", the output should be
+createAnagram(s, t) = 1;
+For s = "OVGHK" and t = "RPGUC", the output should be
+createAnagram(s, t) = 4.
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string s
+
+Guaranteed constraints:
+5 ≤ s.length ≤ 35.
+
+[input] string t
+
+Guaranteed constraints:
+t.length = s.length.
+
+[output] integer
+
+The minimum number of replacement operations needed to get an anagram of the string t from the string s.
+
+        */
+
+        public
+        int createAnagram(string s, string t) {
+            foreach(char u in s) {
+                if (t.IndexOf(u) != -1) {
+                    t = t.Remove(t.IndexOf(u), 1);
+                }//if (t.IndexOf(u) != -1) {
+            }//foreach(char u in s) {
+            return t.Length;
+        }//int createAnagram(string s, string t) {
+    }//public class CreateAnagram {
+}//namespace Algorithms.src._1_the_core._07_mirror_lake {
