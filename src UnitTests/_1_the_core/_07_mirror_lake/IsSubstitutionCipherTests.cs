9e02cce046fceab3525fbe6db4fdf6da193586c9
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-07-04 02:49:53 -0500

    IsSubstitutionCipher.cs

---
+++ src UnitTests/_1_the_core/_07_mirror_lake/IsSubstitutionCipherTests.cs
@@ -0,0 +1,88 @@
+﻿using Algorithms.src._1_the_core._07_mirror_lake;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
+    [TestClass()]
+    public class IsSubstitutionCipherTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase1 = "aacb";
+            var testCase2 = "aabc";
+            Test(testCase1, testCase2, true);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase1 = "aa";
+            var testCase2 = "bc";
+            Test(testCase1, testCase2, false);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase1 = "aaxxaaz";
+            var testCase2 = "aazzaay";
+            Test(testCase1, testCase2, true);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase1 = "aaxyaa";
+            var testCase2 = "aazzaa";
+            Test(testCase1, testCase2, false);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase1 = "aabc";
+            var testCase2 = "aacb";
+            Test(testCase1, testCase2, true);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase1 = "dccd";
+            var testCase2 = "zzxx";
+            Test(testCase1, testCase2, false);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase1 = "ddcc";
+            var testCase2 = "zzxx";
+            Test(testCase1, testCase2, true);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase1 = "aaaabbbbcc";
+            var testCase2 = "cccccccccc";
+            Test(testCase1, testCase2, false);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase1 = "abcdefg";
+            var testCase2 = "hijklmn";
+            Test(testCase1, testCase2, true);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase1 = "aaabbbccc";
+            var testCase2 = "aaabbbccc";
+            Test(testCase1, testCase2, true);
+        }//public void Test10() {
+
+        private void Test(string testCase1, string testCase2, bool solution) {
+            if (solution) {
+                Assert.IsTrue(new IsSubstitutionCipher().isSubstitutionCipher(testCase1, testCase2));
+            } else {//if (solution) {
+                Assert.IsFalse(new IsSubstitutionCipher().isSubstitutionCipher(testCase1, testCase2));
+            }//else {
+        }//private void Test(string testCase1, string testCase2, bool solution) {
+    }//public class IsSubstitutionCipherTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
---
+++ src/_1_the_core/_07_mirror_lake/IsSubstitutionCipher.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms.src._1_the_core._07_mirror_lake {
+    public class IsSubstitutionCipher {
+
+        /*
+
+A ciphertext alphabet is obtained from the plaintext alphabet by means of rearranging some characters. For example "bacdef...xyz" will be a simple ciphertext alphabet where a and b are rearranged.
+
+A substitution cipher is a method of encoding where each letter of the plaintext alphabet is replaced with the corresponding (i.e. having the same index) letter of some ciphertext alphabet.
+
+Given two strings, check whether it is possible to obtain them from each other using some (possibly, different) substitution ciphers.
+
+Example
+
+For string1 = "aacb" and string2 = "aabc", the output should be
+isSubstitutionCipher(string1, string2) = true.
+
+Any ciphertext alphabet that starts with acb... would make this transformation possible.
+
+For string1 = "aa" and string2 = "bc", the output should be
+isSubstitutionCipher(string1, string2) = false.
+
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string string1
+
+A string consisting of lowercase English characters.
+
+Guaranteed constraints:
+1 ≤ string1.length ≤ 10.
+
+[input] string string2
+
+A string consisting of lowercase English characters of the same length as string1.
+
+Guaranteed constraints:
+string2.length = string1.length.
+
+[output] boolean
+
+        */
+
+        public
+        bool isSubstitutionCipher(string string1, string string2) {
+            Dictionary<char, char> ht = new Dictionary<char,char>();
+            Dictionary<char, char> th = new Dictionary<char, char>();
+            for(int i = 0; i < string1.Length; i++) {
+                if ((!ht.TryAdd(string1[i], string2[i]) && ht[string1[i]] != string2[i]) | (!th.TryAdd(string2[i], string1[i]) && th[string2[i]] != string1[i])) {
+                    return false;
+                }//if ((!ht.TryAdd(string1[i], string2[i]) && ht[string1[i]] != string2[i]) | (!th.TryAdd(string2[i], string1[i]) && th[string2[i]] != string1[i])) {
+            }//for(int i = 0; i < string1.Length; i++) {
+            return true;
+        }//bool isSubstitutionCipher(string string1, string string2) {
+    }//public class IsSubstitutionCipher {
+}//namespace Algorithms.src._1_the_core._07_mirror_lake {
