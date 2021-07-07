dbc6cce35e9c4ed283d6be5d1fc6f68456550b6c
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-07-07 08:44:31 -0500

    StringsConstruction.cs

---
+++ src UnitTests/_1_the_core/_07_mirror_lake/StringsConstructionTests.cs
@@ -0,0 +1,182 @@
+﻿using Algorithms.src._1_the_core._07_mirror_lake;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
+    [TestClass()]
+    public class StringsConstructionTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase1 = "abc";
+            var testCase2 = "abccba";
+            var solution = 2;
+            Test(testCase1, testCase2, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase1 = "ab";
+            var testCase2 = "abcbcb";
+            var solution = 1;
+            Test(testCase1, testCase2, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase1 = "ab";
+            var testCase2 = "abccba";
+            var solution = 2;
+            Test(testCase1, testCase2, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase1 = "b";
+            var testCase2 = "abccba";
+            var solution = 2;
+            Test(testCase1, testCase2, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase1 = "c";
+            var testCase2 = "abccbac";
+            var solution = 3;
+            Test(testCase1, testCase2, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase1 = "z";
+            var testCase2 = "z";
+            var solution = 1;
+            Test(testCase1, testCase2, solution);
+        }//public void Test6() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test7() {
+            var testCase1 = "z";
+            var testCase2 = "y";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test7() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test8() {
+            var testCase1 = "za";
+            var testCase2 = "bzc";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test8() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test9() {
+            var testCase1 = "hnccv";
+            var testCase2 = "hncvohcjhdfnhonxddcocjncchnvohchnjohcvnhjdhihsn";
+            var solution = 3;
+            Test(testCase1, testCase2, solution);
+        }//public void Test9() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test10() {
+            var testCase1 = "abc";
+            var testCase2 = "def";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test10() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test11() {
+            var testCase1 = "zzz";
+            var testCase2 = "zzzzzzzzzzz";
+            var solution = 3;
+            Test(testCase1, testCase2, solution);
+        }//public void Test11() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test12() {
+            var testCase1 = "abcabcabc";
+            var testCase2 = "aaaaaaaaaaabbbbbbbbbbcccccccccc";
+            var solution = 3;
+            Test(testCase1, testCase2, solution);
+        }//public void Test12() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test13() {
+            var testCase1 = "abc";
+            var testCase2 = "xyz";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test13() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test14() {
+            var testCase1 = "zbc";
+            var testCase2 = "ydlblksdjccdddb";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test14() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test15() {
+            var testCase1 = "abdd";
+            var testCase2 = "adadapqrtsmnckgljj";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test15() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test16() {
+            var testCase1 = "abcde";
+            var testCase2 = "edbcaacbdebcedaadbecadbceecabddbaecabecdcdabeabcde";
+            var solution = 10;
+            Test(testCase1, testCase2, solution);
+        }//public void Test16() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test17() {
+            var testCase1 = "abcde";
+            var testCase2 = "edbcaaclpebcekbadbecadbcefcabddbaecaaaaacdakrabcde";
+            var solution = 7;
+            Test(testCase1, testCase2, solution);
+        }//public void Test17() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test18() {
+            var testCase1 = "zkz";
+            var testCase2 = "zpaskazgazkazggaaksiokkzzzzaaazzpkuazzzgzkzabrgzzq";
+            var solution = 7;
+            Test(testCase1, testCase2, solution);
+        }//public void Test18() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test19() {
+            var testCase1 = "ggagagagga";
+            var testCase2 = "gga";
+            var solution = 0;
+            Test(testCase1, testCase2, solution);
+        }//public void Test19() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test20() {
+            var testCase1 = "pqrsttt";
+            var testCase2 = "parfkajfraqrakjssattertjeqsqaptatdptdstasreraqprtt";
+            var solution = 3;
+            Test(testCase1, testCase2, solution);
+        }//public void Test20() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test21() {
+            var testCase1 = "azgtpzzgab";
+            var testCase2 = "taagzajfrqrakjzztgzgzazstppatdptdblgaasreraqprtt";
+            var solution = 1;
+            Test(testCase1, testCase2, solution);
+        }//public void Test21() {
+
+        private void Test(string testCase1, string testCase2, int solution) {
+            Assert.AreEqual(solution, new StringsConstruction().stringsConstruction(testCase1, testCase2));
+        }//private void Test(string testCase1, string testCase2, int solution) {
+    }//public class StringsConstructionTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
---
+++ src/_1_the_core/_07_mirror_lake/StringsConstruction.cs
@@ -0,0 +1,54 @@
+﻿namespace Algorithms.src._1_the_core._07_mirror_lake {
+    public class StringsConstruction {
+
+        /*
+
+Given two strings a and b, both consisting only of lowercase English letters, your task is to calculate how many strings equal to a can be constructed using only letters from the string b? Each letter can be used only once and in one string only.
+
+Example
+
+For a = "abc" and b = "abccba", the output should be stringsConstruction(a, b) = 2.
+
+We can construct 2 strings a = "abc" using only letters from the string b.
+
+For a = "ab" and b = "abcbcb", the output should be stringsConstruction(a, b) = 1.
+
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string a
+
+String to construct, containing only lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 105.
+
+[input] string b
+
+String containing needed letters, containing only lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ b.length ≤ 105.
+
+[output] integer
+
+The number of strings a that can be constructed from the string b.
+
+        */
+
+        public
+        int stringsConstruction(string a, string b) {
+            int output = b.Split(a[0]).Length - 1;
+            for(char i = 'a'; i <= 'z'; i++) {
+                if (a.Split(i).Length > 1) {
+                    int tempOut = (b.Split(i.ToString()).Length - 1) / (a.Split(i).Length - 1);
+                    if (output > tempOut) {
+                        output = tempOut;
+                    }//if (output > tempOut) {
+                }//if (a.Split(i).Length > 1) {
+            }//for(char i = 'a'; i <= 'z'; i++) {
+            return output;
+        }//int stringsConstruction(string a, string b) {
+    }//public class StringsConstruction {
+}//namespace Algorithms.src._1_the_core._07_mirror_lake {
