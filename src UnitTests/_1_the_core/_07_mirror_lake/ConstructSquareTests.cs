e5ec94e3a5180ef3903d25d9a331a942d8645f5c
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-07-20 23:32:34 -0500

    ConstrucSquares.cs

---
+++ src UnitTests/_1_the_core/_07_mirror_lake/ConstructSquareTests.cs
@@ -0,0 +1,56 @@
+﻿using Algorithms.src._1_the_core._07_mirror_lake;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
+    [TestClass()]
+    public class ConstructSquareTests {
+
+        private const int timeout = 3000;
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test1() {
+            var testCase = "ab";
+            var solution = 81;
+            Test(testCase, solution);
+        }//public void Test1() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test2() {
+            var testCase = "zzz";
+            var solution = -1;
+            Test(testCase, solution);
+        }//public void Test2() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test3() {
+            var testCase = "aba";
+            var solution = 900;
+            Test(testCase, solution);
+        }//public void Test3() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test4() {
+            var testCase = "abcbbb";
+            var solution = 810000;
+            Test(testCase, solution);
+        }//public void Test4() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test5() {
+            var testCase = "abc";
+            var solution = 961;
+            Test(testCase, solution);
+        }//public void Test5() {
+
+        [TestMethod(), Timeout(timeout)]
+        public void Test6() {
+            var testCase = "aaaabbcde";
+            var solution = 999950884;
+            Test(testCase, solution);
+        }//public void Test6() {
+
+        private void Test(string testCase, int solution) {
+            Assert.AreEqual(solution, new ConstructSquare().constructSquare(testCase));
+        }//private void Test(string testCase, int solution) {
+    }//public class ConstructSquareTests {
+}//namespace Algorithms.src_UnitTests._1_the_core._07_mirror_lake {
---
+++ src/_1_the_core/_07_mirror_lake/ConstructSquare.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.src._1_the_core._07_mirror_lake {
+    public class ConstructSquare {
+
+        /*
+
+Given a string consisting of lowercase English letters, find the largest square number which can be obtained by reordering the string's characters and replacing them with any digits you need (leading zeros are not allowed) where same characters always map to the same digits and different characters always map to different digits.
+
+If there is no solution, return -1.
+
+Example
+
+For s = "ab", the output should be
+constructSquare(s) = 81.
+The largest 2-digit square number with different digits is 81.
+For s = "zzz", the output should be
+constructSquare(s) = -1.
+There are no 3-digit square numbers with identical digits.
+For s = "aba", the output should be
+constructSquare(s) = 900.
+It can be obtained after reordering the initial string into "baa" and replacing "a" with 0 and "b" with 9.
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string s
+
+Guaranteed constraints:
+1 ≤ s.length < 10.
+
+[output] integer
+
+        */
+
+        public
+        int constructSquare(string s) {
+            s = string.Concat(s.ToString().OrderBy(c => c * s.ToString().Split(c).Length));
+            int max = (int)Math.Pow(10.0, s.Length);
+            double sqrtMax = Math.Sqrt(max);
+            for (int i = (int)(sqrtMax % 10 == 0 ? sqrtMax - 1 : sqrtMax); i * i >= max / 10; i--) {
+                if (IsSubstitutionCipher(s, string.Concat((i * i).ToString().OrderBy(c => c * (i * i).ToString().Split(c).Length)))) {
+                    return i * i;
+                }//if (IsSubstitutionCipher(s, string.Concat((i * i).ToString().OrderBy(c => c * (i * i).ToString().Split(c).Length)))) {
+            }//for (int i = (int)(sqrtMax % 10 == 0 ? sqrtMax - 1 : sqrtMax); i * i >= max / 10; i--) {
+            return -1;
+        }//int constructSquare(string s) {
+
+        private bool IsSubstitutionCipher(string string1, string string2) {
+            Dictionary<char, char> ht = new Dictionary<char, char>();
+            Dictionary<char, char> th = new Dictionary<char, char>();
+            for (int i = 0; i < string1.Length; i++) {
+                if ((!ht.TryAdd(string1[i], string2[i]) && ht[string1[i]] != string2[i]) | (!th.TryAdd(string2[i], string1[i]) && th[string2[i]] != string1[i])) {
+                    return false;
+                }//if ((!ht.TryAdd(string1[i], string2[i]) && ht[string1[i]] != string2[i]) | (!th.TryAdd(string2[i], string1[i]) && th[string2[i]] != string1[i])) {
+            }//for (int i = 0; i < string1.Length; i++) {
+            return true;
+        }//private bool IsSubstitutionCipher(string string1, string string2) {
+    }//public class ConstructSquare {
+}//namespace Algorithms.src._1_the_core._07_mirror_lake {
